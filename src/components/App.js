a1efbd254987008909236469404e2c9fd4fcbf2f
Casey Shamey <caseyshamey@gmail.com>
2020-08-03 16:51:06 -0400

    Initial React setup

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+
+  }
+
+  render() {
+    return(
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
+
+export default App;
