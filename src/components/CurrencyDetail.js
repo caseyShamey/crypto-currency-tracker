7c65949b1dbefe9b01000ad1738f6449533f9b64
Casey Shamey <caseyshamey@gmail.com>
2020-08-15 15:59:51 -0400

    Format currency detail, add relative dates, add commas to nums, add NA for undefined values

--- src/components/CurrencyDetail.js
+++ src/components/CurrencyDetail.js
@@ -1,25 +1,52 @@
 import React from 'react';
+import TimeAgo from 'timeago-react';
+
+
 import "./CurrencyDetail.css";
 
 const CurrencyDetail = ({ currency }) => {
+  let { name, symbol, rank, price, price_timestamp, high, high_timestamp, circulating_supply, max_supply, market_cap, logo_url } = currency
+
+  const numWithCommas = (num) => {
+    if (num === undefined) {
+      return 'NA'
+    } else {
+    let num_parts = num.toString().split(".");
+    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    return num_parts.join(".");
+    }
+  }
+
   return (
-    <div>
-      {currency.id}
+    <div className="currency-detail-container">
+      <div className="detail-image-container">
+        <img className="detail-logo" alt={name} src={logo_url} />
+      </div>
+      <div className="name-container">
+        <h1>{name} ({symbol})</h1>
+      </div>
+      <div className="info-container">
+        <h3>Rank: {rank}</h3>
+        <h3>
+          Price: ${numWithCommas(price)} as of <span> </span>
+          <TimeAgo
+            datetime={price_timestamp}
+          />
+        </h3>
+        <h3>
+          High price of ${numWithCommas(high)} was reached <span> </span>
+          <TimeAgo
+            datetime={high_timestamp}
+          />
+        </h3>
+        <h3>Circulating Supply: {numWithCommas(circulating_supply)}</h3>
+        <h3>Max Supply: {numWithCommas(max_supply)}</h3>
+        <h3>Market Cap: {numWithCommas(market_cap)}</h3>
+      </div>
     </div>
   )
 }
 
 export default CurrencyDetail;
 
-/*
-logo
-currency name     symbol
-rank
-price       as of ____ ago
-high price of _____  was reached on
-
-circulating supply
-max supply
-
-market cap
-*/
